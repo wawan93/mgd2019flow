8810472ab0220e05716c4d03074cf3d0d179a338
Vladimir Kochergin <v.kochergin@raftlab.ru>
2019-05-29 22:07:59 +0300

    fix show info

--- resources/views/admin/collector/show.blade.php
+++ resources/views/admin/collector/show.blade.php
@@ -24,10 +24,29 @@
                         <div class="table-responsive">
                             <table class="table">
                                 <tbody>
+                                <?php
+                                        /** @var \App\Collector $collector */
+                                ?>
                                     <tr>
                                         <th>ID</th><td>{{ $collector->id }}</td>
                                     </tr>
-                                    <tr><th> Name </th><td> {{ $collector->name }} </td></tr><tr><th> Surname </th><td> {{ $collector->surname }} </td></tr><tr><th> Status </th><td> {{ $collector->status }} </td></tr>
+                                    <tr><th> Имя </th><td> {{ $collector->name }} </td></tr>
+                                    <tr><th> Фамилия </th><td> {{ $collector->surname }} </td></tr>
+                                    <tr><th> Отчество </th><td> {{ $collector->middlename }} </td></tr>
+                                    <tr><th> Дата рождения </th><td> {{ $collector->birthday }} </td></tr>
+                                    <tr><th> Email </th><td> {{ $collector->email }} </td></tr>
+                                    <tr><th> Телефон </th><td> {{ $collector->phone }} </td></tr>
+                                    <tr><th> Гражданство РФ </th><td> {{ $collector->has_citizenship }} </td></tr>
+                                    <tr><th> О себе </th><td> {{ $collector->aboutself }} </td></tr>
+                                    <tr><th> fb </th><td> {{ $collector->social_fb }} </td></tr>
+                                    <tr><th> twitter </th><td> {{ $collector->social_tw }} </td></tr>
+                                    <tr><th> vk </th><td> {{ $collector->social_vk }} </td></tr>
+                                    <tr><th> IP </th><td> {{ $collector->reg_ip }} </td></tr>
+                                    <tr><th> Referrer </th><td> {{ $collector->origin }} </td></tr>
+                                    <tr><th> ok_about_myinfo </th><td> {{ $collector->ok_about_myinfo }} </td></tr>
+                                    <tr><th> Дата регистрации </th><td> {{ $collector->date }} </td></tr>
+                                    <tr><th> UTM </th><td> {{ $collector->utm_list }} </td></tr>
+                                    <tr><th> Status </th><td> {{ $collector->status }} </td></tr>
                                 </tbody>
                             </table>
                         </div>
