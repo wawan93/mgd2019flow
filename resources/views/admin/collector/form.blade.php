a0b2fa329077b867832554336a26ff9afa395b7e
Vladimir Kochergin <v.kochergin@raftlab.ru>
2019-05-29 22:07:47 +0300

    fix form

--- resources/views/admin/collector/form.blade.php
+++ resources/views/admin/collector/form.blade.php
@@ -5,11 +5,11 @@
 </div>
 <div class="form-group {{ $errors->has('surname') ? 'has-error' : ''}}">
     <label for="surname" class="control-label">{{ 'Surname' }}</label>
-    <textarea class="form-control" rows="5" name="surname" type="textarea" id="surname" >{{ isset($collector->surname) ? $collector->surname : ''}}</textarea>
+    <input class="form-control" name="surname" type="text" id="surname" value="{{ isset($collector->surname) ? $collector->surname : ''}}" >
     {!! $errors->first('surname', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('status') ? 'has-error' : ''}}">
-    <label for="status" class="control-label">{{ 'Status' }}</label>
+    <label for="status" class="control-label">{{ 'Статус' }}</label>
     <select name="status" class="form-control" id="status" >
     @foreach (json_decode('{"new":"\u041d\u0435 \u043f\u0440\u043e\u0432\u0435\u0440\u0435\u043d","research":"\u0432 \u0440\u0438\u0441\u0451\u0440\u0447\u0435","research_done":"\u043f\u0440\u043e\u0448\u0451\u043b \u0440\u0438\u0441\u0451\u0440\u0447","interview_accespted":"\u0437\u0430\u043f\u0438\u0441\u0430\u043d \u043d\u0430 \u0441\u043e\u0431\u0435\u0441\u0435\u0434\u043e\u0432\u0430\u043d\u0438\u0435","accepted":"\u043f\u0440\u0438\u043d\u044f\u0442","declined":"\u043e\u0442\u043a\u043b\u043e\u043d\u0451\u043d","fired":"\u0443\u0432\u043e\u043b\u0435\u043d"}', true) as $optionKey => $optionValue)
         <option value="{{ $optionKey }}" {{ (isset($collector->status) && $collector->status == $optionKey) ? 'selected' : ''}}>{{ $optionValue }}</option>
