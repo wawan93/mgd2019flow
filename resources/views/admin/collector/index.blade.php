0ca35b6e05a6c966b877884556138cc8eada1516
Vladimir Kochergin <v.kochergin@raftlab.ru>
2019-05-29 22:09:59 +0300

    remove delete button

--- resources/views/admin/collector/index.blade.php
+++ resources/views/admin/collector/index.blade.php
@@ -41,12 +41,6 @@
                                         <td>
                                             <a href="{{ url('/admin/collector/' . $item->id) }}" title="View Collector"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
                                             <a href="{{ url('/admin/collector/' . $item->id . '/edit') }}" title="Edit Collector"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-
-                                            <form method="POST" action="{{ url('/admin/collector' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Collector" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                                            </form>
                                         </td>
                                     </tr>
                                 @endforeach
--- resources/views/admin/collector/show.blade.php
+++ resources/views/admin/collector/show.blade.php
@@ -13,11 +13,6 @@
                         <a href="{{ url('/admin/collector') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
                         <a href="{{ url('/admin/collector/' . $collector->id . '/edit') }}" title="Edit Collector"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
-                        <form method="POST" action="{{ url('admin/collector' . '/' . $collector->id) }}" accept-charset="UTF-8" style="display:inline">
-                            {{ method_field('DELETE') }}
-                            {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-sm" title="Delete Collector" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                        </form>
                         <br/>
                         <br/>
 
