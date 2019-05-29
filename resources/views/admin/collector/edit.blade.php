efb8be7affc871c59bb317091bd1ef62d08c4ea8
Vladimir Kochergin <v.kochergin@raftlab.ru>
2019-05-30 01:54:12 +0300

    remove sidebar

--- resources/views/admin/collector/edit.blade.php
+++ resources/views/admin/collector/edit.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            @include('admin.sidebar')
-
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Edit Collector #{{ $collector->id }}</div>
                     <div class="card-body">
